08ad7c4cc22f2a5a23c5fd0bcbf2eea9db96e6fc
github username <github email address>
2020-05-02 00:05:00 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const taskForm = document.getElementById('create-task-form');
+//   const taskDescription = document.getElementById('new-task-description');
+//   const taskList = document.getElementById('tasks')
+
+//   taskForm.addEventListener("submit", taskCreation(event));
+  
+
+//   function taskCreation(event){
+//     event.preventDefault();
+//     let newTask = document.createElement("li");
+//     newTask.innerText = taskDescription.value; 
+    
+    
+
+//     const appendNewTask = event => {
+//       document.getElementById("tasks").appendChild(event);
+//     }
+    
+//     appendNewTask(newTask)
+
+    
+//   }
+  
+// });
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  // const newTaskDescription = document.getElementById("new-task-description");
+  // const newTaskPriority = document.getElementById("new-task-priority");
+  const newTaskUl = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  //stop form from trying to submit
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
+debugger
