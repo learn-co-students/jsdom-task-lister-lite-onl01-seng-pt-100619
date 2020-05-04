0a59179061877fa1eba88e05c165d48ab7e5a825
Phil Billingsby <philbillingsby@gmail.com>
2020-05-04 09:39:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('#create-task-form').addEventListener("submit", function(e) {
-    let text_input = document.getElementById('new-task-description').value;
-    document.getElementById('tasks').innerHTML += `<li> ${text_input} <a href="#" onclick="parentNode.parentNode.removeChild(parentNode)">Delete</a></li>`;
+    let textInput = document.getElementById('new-task-description').value;
+    document.getElementById('tasks').innerHTML += `<li> ${textInput} <a href="#" onclick="parentNode.parentNode.removeChild(parentNode)">Delete</a></li>`;
     e.preventDefault();
   });
 });
