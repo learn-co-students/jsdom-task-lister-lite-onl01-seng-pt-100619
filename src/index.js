b437e81e62f7a84385476bdeb70d4885248ab0b4
steelerx155 <steelerx155@gmail.com>
2020-05-08 11:56:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log("Hello")
+    document.getElementById("list")    
+}, false);
