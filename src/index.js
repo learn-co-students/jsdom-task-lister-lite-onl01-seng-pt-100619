173d0d677fe5a287f1914bf23f4a4498594d1b01
justin <jayman1294@gmail.com>
2020-05-11 21:48:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
 });
+
+const form = document.querySelector("#create-task-form")
+const list = document.querySelector("#tasks")
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault()
+
+const description = document.querySelector("#new-task-description").value
+
+list.innerHTML += ` <li> ${description} <button onclick="Delete(this)">Delete</button> </li> `
+
+
+})
+
+
+function Delete(currentEl){
+currentEl.parentNode.parentNode.removeChild(currentEl.parentNode);
+}
+
+
+
+
+
+
+
+
+
+
