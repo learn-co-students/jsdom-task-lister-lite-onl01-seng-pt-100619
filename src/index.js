cc71287440d742a12cf5b501b460333d960b0903
Phil Billingsby <philbillingsby@gmail.com>
2020-06-27 21:32:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector('#create-task-form').addEventListener("submit", function(e) {
-    let textInput = document.getElementById('new-task-description').value;
-    document.getElementById('tasks').innerHTML += `<li> ${textInput} <a href="#" onclick="parentNode.parentNode.removeChild(parentNode)">Delete</a></li>`;
-    e.preventDefault();
-  });
+document.addEventListener("DOMContentLoaded", (e) => {
+  getAndDisplayTodo()
 });
+
+let getAndDisplayTodo = function(e) {
+  document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let content = document.getElementById('new-task-description').value
+    let li = document.createElement('li');
+    li.innerHTML += `<p>${content}</p> <a href="#" onclick="deleteLi(this)">Delete</a> <select onchange="priority(this)"><option value="red">Red</option><option value="yellow">Yellow</option><option value="green">Green</option></select>`
+    document.getElementById('tasks').appendChild(li);
+  })
+}
+
+function deleteLi(link) {
+  link.parentNode.parentNode.removeChild(link.parentNode);
+}
+
+function priority(link) {
+  link.parentElement.querySelector('p').style.color = link.value
+}
+
+
