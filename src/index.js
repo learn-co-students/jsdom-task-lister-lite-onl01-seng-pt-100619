bff1b32466543c49344f5c05c669d6ae193f72a0
kelly_maass <kmaass77627@gmail.com>
2023-07-06 21:09:40 -0500

    new

--- src/index.js
+++ src/index.js
@@ -1,20 +1,50 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let form = document.getElementById("create-task-form")
-    console.log("this is working",form)
+let form = document.getElementById("create-task-form")
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  addtodom(e.target[0].value)
   
-    let textBox =  document.getElementById("new-task-description")
+})
+
+function addtodom(value) {
+  let para = document.createElement('p')
+  para.textContent = value
+  let butn = document.createElement('button')
+  butn.addEventListener('click', fordelete)
+  butn.textContent = 'delete'
+  para.appendChild(butn)
+ let parent = document.querySelector("#list")//.appendChild(para)
+ parent.appendChild(para)
+}
+
+function fordelete(e) {
+  console.log(e)
+  e.target.parentNode.remove()
+}
+
+
+
+
+
+
+
+
+//document.addEventListener("DOMContentLoaded", () => {
+ //let form = document.getElementById("create-task-form")
+   // console.log("this is working",form)
+  
+    //let textBox =  document.getElementById("new-task-description")
     // let button = form.querySelectorAll('input')[1]
     
-    let tasks = document.getElementById('tasks') // ul
+    //let tasks = document.getElementById('tasks') // ul
     
-    form.addEventListener("submit", function(event) {
-      event.preventDefault()
-      console.log("button clicked")
-      let li = document.createElement("li")
-      li.innerText = textBox.value    //form.innerText
-      tasks.append(li)
-    })
-});
+    //form.addEventListener("submit", function(event) {
+     // event.preventDefault()
+      //console.log("button clicked")
+     // let li = document.createElement("li")
+      //li.innerText = textBox.value    //form.innerText
+      //tasks.append(li)
+    //})
+//});
 
 
 
